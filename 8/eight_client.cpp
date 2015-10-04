79bef6caf0d9432b1a4f0c6c95e61c48236043f8
Clock <double7@mail.ru>
2015-10-04 19:43:50 +0600

    done

---
+++ 8/eight_client.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "mppp.cpp"
+
+/*
+#include <>
+#include <>
+#include <>
+*/
+
+// https://stackoverflow.com/questions/8126512/deprecated-conversion-from-string-constant-to-char про строки и чары
+
+
+
+int main(int argc, char *argv[]){
+	if(argc < 2){
+		cout << "ERROR: First argument must be IP address\n";
+		return 0;
+	}
+	mppp client(43690, 1, argv[1]);
+	
+	
+	return 0;
+}
---
+++ 8/eight_server.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "mppp.cpp"
+/*
+#include <>
+#include <>
+#include <>
+*/
+
+// https://stackoverflow.com/questions/8126512/deprecated-conversion-from-string-constant-to-char про строки и чары
+
+
+int main(){
+	mppp server(43690, 0);
+	return 0;
+}
